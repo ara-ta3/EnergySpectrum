4a60349d601aee33ddb9aa2b21a3441dc3edd980
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-07-07 15:33:50 +0900

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+application {
+    mainClass.set("spectrum.energy.MainKt")
+}
---
+++ src/main/kotlin/spectrum/energy/Main.kt
@@ -0,0 +1,6 @@
+package spectrum.energy
+
+
+fun main() {
+    println("Hello, World!")
+}
