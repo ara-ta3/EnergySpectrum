c6d0aa053150763593d93ee431bdae6d017668ea
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-07-07 15:43:04 +0900

    Add build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 application {
